32375fb9aa63e71b4af981dfbf2a5d919478d6f2
JorgeIvanPuyo <ji.puyo@uniandes.edu.co>
2025-04-28 23:03:01 -0500

    change jsonable_encoder(db_obj) to obj_in.dict(exclude_unset=True) in repository base to receive dates

--- app/db/repositories/base.py
+++ app/db/repositories/base.py
@@ -96,13 +96,22 @@ class CRUDBaseRepository(Generic[ModelType, CreateSchemaType, UpdateSchemaType])
         result = await db.execute(select(self.model).offset(skip).limit(limit))
         return result.scalars().all()
 
+    # async def create(self, db: AsyncSession, obj_in: CreateSchemaType) -> ModelType:
+    #     obj_in_data = jsonable_encoder(obj_in)
+    #     db_obj = self.model(**obj_in_data)
+    #     db.add(db_obj)
+    #     await db.commit()
+    #     await db.refresh(db_obj)
+    #     return db_obj
+
     async def create(self, db: AsyncSession, obj_in: CreateSchemaType) -> ModelType:
-        obj_in_data = jsonable_encoder(obj_in)
+        obj_in_data = obj_in.dict(exclude_unset=True)
         db_obj = self.model(**obj_in_data)
         db.add(db_obj)
         await db.commit()
         await db.refresh(db_obj)
         return db_obj
+
 
     async def update(self, db: AsyncSession, db_obj: ModelType, obj_in: Union[UpdateSchemaType, Dict[str, Any]]) -> ModelType:
         obj_data = jsonable_encoder(db_obj)
