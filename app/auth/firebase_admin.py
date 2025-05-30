7e2de5ee874ae156032f6132da4a894c487bd71a
Cristhian <ccdelgadop@gmail.com>
2025-05-29 22:36:57 -0500

    Enhance CI workflow and Firebase admin integration
    - Added FIREBASE_SERVICE_ACCOUNT_JSON to environment variables in CI workflow for deployment.
    - Implemented logic in firebase_admin.py to create a local file for Firebase credentials if it doesn't exist, using the provided environment variable.

--- app/auth/firebase_admin.py
+++ app/auth/firebase_admin.py
@@ -5,7 +5,14 @@ from firebase_admin import credentials
 import os
 from pathlib import Path
 
+firebase_json = os.getenv("FIREBASE_SERVICE_ACCOUNT_JSON")
+
 default_path = Path("firebase-service-account.json")
+
+if firebase_json and not default_path.exists():
+    with open(default_path, "w") as f:
+        f.write(firebase_json)
+        
 firebase_cred_path = os.getenv("FIREBASE_CREDENTIALS_PATH", default_path)
 
 if not firebase_admin._apps:
