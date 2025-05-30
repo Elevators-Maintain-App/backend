b306add93f9b3942b7e928a65501fd0ea00ad586
Cristhian <ccdelgadop@gmail.com>
2025-05-29 23:16:03 -0500

    Refactor Firebase credentials handling in firebase_admin.py
    - Updated logic to decode base64 encoded Firebase credentials and write them to a local file only if it doesn't already exist, improving clarity and functionality.

--- app/auth/firebase_admin.py
+++ app/auth/firebase_admin.py
@@ -10,9 +10,10 @@ firebase_json_b64 = os.getenv("FIREBASE_SERVICE_ACCOUNT_B64")
 
 default_path = Path("firebase-service-account.json")
 
-if firebase_json_b64 and not default_path.exists():    
+if firebase_json_b64 and not default_path.exists():
+    firebase_json = base64.b64decode(firebase_json_b64).decode("utf-8")
     with open(default_path, "w") as f:
-        f.write(base64.b64decode(firebase_json_b64))
+        f.write(firebase_json)
 
 firebase_cred_path = os.getenv("FIREBASE_CREDENTIALS_PATH", default_path)
 
