4969ac906464391654af1a36ac68e4eaf3277628
JorgeIvanPuyo <ji.puyo@uniandes.edu.co>
2025-06-21 19:03:53 -0500

    Update enum order states

--- app/utils/estados_orden.py
+++ app/utils/estados_orden.py
@@ -1,7 +1,8 @@
 # app/utils/estados_orden.py
 class EstadoOrdenID:
     PENDIENTE     = 1
-    ASIGNADA      = 4
-    EN_EJECUCION  = 5
-    EN_PAUSA      = 34
-    COMPLETADA    = 3
+    ASIGNADA      = 2
+    EN_EJECUCION  = 3
+    EN_VALIDACION = 4
+    COMPLETADA    = 5
+    EN_PAUSA      = 6
